a8e67c3f97d8e7c82be344321a66a376f78e1397
Mohammed Abufarhana <realmohaa@mailbox.unideb.hu>
2022-01-01 03:39:44 +0100

    Added Intents between both Activites

--- app/src/main/java/com/example/textim/activities/LogInActivity.java
+++ app/src/main/java/com/example/textim/activities/LogInActivity.java
@@ -2,15 +2,27 @@ package com.example.textim.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.textim.R;
+import com.example.textim.databinding.ActivityLogInBinding;
 
 public class LogInActivity extends AppCompatActivity {
+
+    // ActivityLogInBinding Automatically generated from activity_log_in
+    private ActivityLogInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        binding = ActivityLogInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.RegisterFromLogin.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), RegistrationActivity.class)));
     }
 }
--- app/src/main/java/com/example/textim/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/textim/activities/RegistrationActivity.java
@@ -2,15 +2,25 @@ package com.example.textim.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.textim.R;
+import com.example.textim.databinding.ActivityRegistrationBinding;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private ActivityRegistrationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.loginFromSignUp.setOnClickListener(v -> onBackPressed());
     }
 }
