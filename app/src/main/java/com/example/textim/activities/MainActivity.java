e33c396ac6735e386f1b6da679c5d42c9cfaca8f
Mohammed Abufarhana <realmohaa@mailbox.unideb.hu>
2022-01-01 16:04:12 +0100

    Main Activity UI, Logout Functionality done

--- app/src/main/java/com/example/textim/activities/MainActivity.java
+++ app/src/main/java/com/example/textim/activities/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.textim.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
+import android.widget.Toast;
 
-import com.example.textim.R;
-import com.example.textim.databinding.ActivityLogInBinding;
 import com.example.textim.databinding.ActivityMainBinding;
 import com.example.textim.utilities.Constants;
 import com.example.textim.utilities.PrefManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +27,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        prefManager = new PrefManager(getApplicationContext());
-        setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        prefManager = new PrefManager(getApplicationContext());
+        setContentView(binding.getRoot());
         fetchUserData();
+        getToken();
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.logout.setOnClickListener(v->logOut());
     }
 
     private void fetchUserData() {
@@ -33,5 +45,43 @@ public class MainActivity extends AppCompatActivity {
         byte[] bytes = Base64.decode(prefManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0 , bytes.length);
         binding.profileImage.setImageBitmap(bitmap);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken(){
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef =
+                db.collection(Constants.KEY_COLLECTION_USERS).document(
+                        prefManager.getString(Constants.KEY_USER_ID)
+                );
+
+        docRef.update(Constants.KEY_FCM_TOKEN,token)
+                .addOnSuccessListener(unused -> showToast("Updated token"))
+                .addOnFailureListener(e -> showToast("Error Updating Token"));
+    }
+
+    private void logOut(){
+        showToast("Logging Out!");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection(Constants.KEY_COLLECTION_USERS).document(
+                prefManager.getString(Constants.KEY_USER_ID)
+        );
+        HashMap<String, Object> ref = new HashMap<>();
+        ref.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        docRef.update(ref)
+                .addOnSuccessListener(unused -> {
+                    prefManager.clear();
+                    startActivity(new Intent(getApplicationContext(), LogInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> showToast("Not able to logout!"));
     }
 }
